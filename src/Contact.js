a6a6f95ac563a5ff91c02d1a684031668adeb658
Ranusha De Silva <ranushadesilva98@gmail.com>
2019-12-22 23:50:42 -0500

    initial contact us page

---
+++ src/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Contact = () => (
+    <div>
+        <h2>Contact Us</h2>
+        <p> Stare at bkdjkfjkajkdgkdfkgsdkgjk dkfjgd dkfjgdjg dfkgjdkgjd fgkdjfkg dkjgd kfjgkdjfg dkjfg kdjfg kdjg dkfjg dkjfg dkjfgk djfgk dfjgk dfjg kdfjg kdfjg kdjf gkdj fkgjd fgjd fjgkd fgkdf gkjdfk gjsgkhgi rhkgjsdk fj skjdfhk jhk ejh fkjsfkesjjjefkhe riugd fjgkd fkjghd kjfhgk dfg kjdhf khjs kgjhskjfhs kdfhdfhgkdgdgdg.df .dfg d.fg.d fg.df.g d.fgdfgdfgdmgndkjfghkdhgdkjgd</p>
+        <p> Stare at bkdjkfjkajkdgkdfkgsdkgjk dkfjgd dkfjgdjg dfkgjdkgjd fgkdjfkg dkjgd kfjgkdjfg dkjfg kdjfg kdjg dkfjg dkjfg dkjfgk djfgk dfjgk dfjg kdfjg kdfjg kdjf gkdj fkgjd fgjd fjgkd fgkdf gkjdfk gjsgkhgi rhkgjsdk fj skjdfhk jhk ejh fkjsfkesjjjefkhe riugd fjgkd fkjghd kjfhgk dfg kjdhf khjs kgjhskjfhs kdfhdfhgkdgdgdg.df .dfg d.fg.d fg.df.g d.fgdfgdfgdmgndkjfghkdhgdkjgd</p>
+        <p> Stare at bkdjkfjkajkdgkdfkgsdkgjk dkfjgd dkfjgdjg dfkgjdkgjd fgkdjfkg dkjgd kfjgkdjfg dkjfg kdjfg kdjg dkfjg dkjfg dkjfgk djfgk dfjgk dfjg kdfjg kdfjg kdjf gkdj fkgjd fgjd fjgkd fgkdf gkjdfk gjsgkhgi rhkgjsdk fj skjdfhk jhk ejh fkjsfkesjjjefkhe riugd fjgkd fkjghd kjfhgk dfg kjdhf khjs kgjhskjfhs kdfhdfhgkdgdgdg.df .dfg d.fg.d fg.df.g d.fgdfgdfgdmgndkjfghkdhgdkjgd</p>
+    </div>
+)
