ccfa794bc9e26e2432337904627eb0aa48f34b28
Ranusha De Silva <ranushadesilva98@gmail.com>
2019-12-22 23:50:09 -0500

    initial about page

---
+++ src/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const About = () => (
+    <div>
+        <h2> About Page </h2>
+        <p> Stare at bkdjkfjkajkdgkdfkgsdkgjk</p>
+        <p> Stare at bkdjkfjkajkdgkdfkgsdkgjk</p>
+        <p> Stare at bkdjkfjkajkdgkdfkgsdkgjk</p>
+    </div>
+)
